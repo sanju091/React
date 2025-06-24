7f2054ccd55c0984fd0404d2787a52e673201ef4
complete-javascript <sanjaychoudhary002005@gmail.com>
2025-06-24 17:40:51 +0530

    add a file

---
+++ 22css-module/learning-passing-children/src/App.jsx
@@ -0,0 +1,24 @@
+import Cointainer from "./components/Container";
+import ErrorMessage from "./components/errorMessage";
+import FoodItems from "./components/foodItems";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+
+const App= () => {
+        //let foodItems=[];
+  let foodItems = ['An item', "A second item", "A third item", "A fourth ite", "And a fifth one"];
+  
+  return (
+    <><Cointainer>
+       <h1>Food-Items</h1>
+    <ErrorMessage items={foodItems}></ErrorMessage>
+    <FoodItems items={foodItems}></FoodItems>
+    </Cointainer>
+     
+    </>
+  );
+};
+
+export default App;
+
